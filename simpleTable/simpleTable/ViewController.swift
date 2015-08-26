6f6f193ba355ebb74e12d468eb5a21c10e849584
tonywks <hsutony@HsuTonydeMacBook-Pro.local>
2015-08-26 16:41:23 +0800

    add  index path

--- simpleTable/simpleTable/ViewController.swift
+++ simpleTable/simpleTable/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    var restaurantNames = ["Cafe Deadend","Homei","Teakha","Cade Loisl","Petite Oyster","For Kee Restaurant",
+        "Po's Atelier","Bourke Street Bakery","Haigh's Chocolate","Palomino Espresso","Upstate","Traif","Graham Avenue Meats And Deli",
+        "Waffle & Wolf","Five Leaves","Cafe Lore","Confessional","Barrafina","Donostia","Royal Oak","CASK Pub and Kitchen"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        return cell
+    }
+    
+    
 
 
 }
