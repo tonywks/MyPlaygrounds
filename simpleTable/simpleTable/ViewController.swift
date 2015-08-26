5b1d8bcd8bf49aa0b6eee64ad23287a5710a4c76
tonywks <hsutony@HsuTonydeMacBook-Pro.local>
2015-08-26 16:10:56 +0800

    New project simpleTable

---
+++ simpleTable/simpleTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleTable
+//
+//  Created by HsuTony on 2015/8/26.
+//  Copyright (c) 2015年 HsuTony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
