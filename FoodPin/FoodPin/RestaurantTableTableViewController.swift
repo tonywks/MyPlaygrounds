b378f94fe4b2e821b9e2486d0ecbf30603f88a13
tonywks <hsutony@HsuTonydeMacBook-Pro.local>
2015-08-27 13:48:25 +0800

    chapter 6 FoodPin

---
+++ FoodPin/FoodPin/RestaurantTableTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RestaurantTableTableViewController.swift
+//  FoodPin
+//
+//  Created by HsuTony on 2015/8/27.
+//  Copyright (c) 2015年 HsuTony. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableTableViewController: UITableViewController {
+
+    var restaurantNames = ["Cafe Deadend","Homei","Teakha","Cade Loisl","Petite Oyster","For Kee Restaurant",
+        "Po's Atelier","Bourke Street Bakery","Haigh's Chocolate","Palomino Espresso","Upstate","Traif",
+        "Graham Avenue Meats And Deli","Waffle & Wolf","Five Leaves","Cafe Lore",
+        "Confessional","Barrafina","Donostia","Royal Oak","CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["1","2","3","4","5","1","2","3","4","5","1","2","3","4","5","1","2","3","4","5","1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return restaurantNames.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifyCell = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifyCell, forIndexPath: indexPath) as! CustomTableViewCell
+
+        cell.nameLabel?.text = restaurantNames[indexPath.row]
+        cell.thumbnailImageView?.image = UIImage(named:restaurantImages[indexPath.row])
+        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.size.width / 2
+//        cell.thumbnailImageView.layer.cornerRadius = 15.0
+//        cell.thumbnailImageView.clipsToBounds = true
+        
+        return cell
+    }
+
+    
+
+}
